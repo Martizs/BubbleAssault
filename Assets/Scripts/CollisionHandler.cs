4ee8452f1253b82071f4bede93ccafb3a3a6e5a4
Martynas <martynas@sdhouse.lt>
2024-01-12 22:33:38 +0200

    Another

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -19,12 +19,21 @@ public class CollisionHandler : MonoBehaviour
     [SerializeField]
     GameObject[] lasers;
 
+    [SerializeField]
+    AudioClip finishSound;
+
+    [SerializeField]
+    ParticleSystem successParticles;
+
+    MusicPLayer musicPLayer;
+
     ScoreBoard scoreBoard;
 
     private void Start()
     {
         starShipAudio = GetComponent<AudioSource>();
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        musicPLayer = FindObjectOfType<MusicPLayer>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -50,6 +59,16 @@ public class CollisionHandler : MonoBehaviour
 
     void InitiateWin()
     {
+        GameObject.FindGameObjectWithTag("MasterTimeline").SetActive(false);
+        musicPLayer.GetComponent<AudioSource>().Stop();
+        musicPLayer.GetComponent<AudioSource>().PlayOneShot(finishSound);
+        successParticles.Play();
+        Invoke("ShowFinishMenu", 3f);
+    }
+
+    void ShowFinishMenu()
+    {
+        Destroy(musicPLayer.gameObject);
         scoreBoard.Finish();
     }
 
@@ -58,6 +77,8 @@ public class CollisionHandler : MonoBehaviour
         GetComponent<MeshRenderer>().enabled = false;
         starShipAudio.Play();
         crashAnimation.SetActive(true);
+
+        scoreBoard.ResetScoreBoard();
 
         Delayed("ReloadScene", afterCrashDelay);
     }
